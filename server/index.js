e850111b67a00424f2728ccc72f2290e16ad344d
Artem Kruhliak <ar.krugliak@gmail.com>
2024-05-02 10:12:12 +0300

    localhost work

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./db");
-const models = require("E:koursachcourseworkservermodelsmodels.js");
+const models = require("./models/models");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (reg, res) => {
+  res.status(200).json({ message: "WORKING!!!" });
+});
 
 const start = async () => {
   try {
