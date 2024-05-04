794591d2d9b3d192f8b4258a388c607a99d0924d
velichko <maksimvelichko16@gmail.com>
2024-05-04 14:16:33 +0300

    added authorization page

---
+++ src/components/footer_login/footer_login.jsx
@@ -0,0 +1,36 @@
+import './footer_login.css'
+import logocard from './../../img/logo/mc_symbol.svg.png'
+import logo from './../../img/logo/logo.png'
+import logo_inst_footer from './../../img/logo/Link → instagram.png.png'
+import logo_telega_footer from './../../img/logo/Link → telegram-footer.svg.png'
+import AnchorLink from '../AnchorLink'
+import { NavLink } from 'react-router-dom'
+
+function FooterLogin(){
+    return(
+        <footer className='footer_login'>
+        <div className="container_footer">
+            <div className="footer_row1">
+                
+            </div>
+            <div className="footer_row2">
+            </div>
+            <div className="footer_row3">
+                <nav className='footer_nav2'>
+                    <ul className="main-links">
+                        <li><img className='logo_footer' src={logo} alt="" /></li>
+                        <li><img className='logo_card_footer' src={logocard} alt="" /></li>
+                        <li><a href="#!">Правила користування</a></li>
+                        <li><a href="#!">Політика конфіденційності</a></li>
+                        <li className='instagram_footer'><a href="https://www.instagram.com/t.kruhliak?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer"><img src={logo_inst_footer} alt="" /></a></li>
+                        <li><a href="https://t.me/kaisteam" target="_blank" rel="noopener noreferrer"><img src={logo_telega_footer} alt="" /></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+    )
+
+}
+
+export default FooterLogin;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
-import Footer from '../components/footer/footer';
+import FooterLogin from '../components/footer_login/footer_login';
 import HeaderLogin from '../components/header_login/header_login';
+
+import logo_google from './../img/logo/google.png'
+
+import AnchorLink from '../components/AnchorLink';
 import './../styles/login.css'
 
 const Login = () => {
@@ -7,9 +11,52 @@ const Login = () => {
         <>
         <HeaderLogin/>
         <div className='login'>
-            Login
+        <body>
+
+            <div className="container_login">
+                <div className="half"></div>
+                <div className="half">
+                    <p className='plog1'>Авторизація</p>
+                    <form className='forms'>
+                    <div className="input-container">
+                        <input type="email" id="email" name="email" placeholder="E-mail"/>
+                    </div>
+                    <div className="input-container">
+                        <input type="password" id="password" name="password" placeholder="Пароль"/>
+                    </div>
+                    </form>
+                    <p className='plog2'>Або увійти за допомогою соціальних мереж</p>
+                    <div className='btn_googl'>
+                    <AnchorLink id="#!" className='google-btn'>
+                      <img src={logo_google} alt="Google logo" className='google-img'/>
+                      Google
+                    </AnchorLink>
+
+                    <form className='checkbox'>
+                        <input type="checkbox" id="remember-me" name="remember-me" className='check_box'/>
+                        <label for="remember-me" className='remember-me'>Запам'ятати мене</label>
+                    </form>
+                    <div className='btns_log'>
+                    <AnchorLink id="#!">
+                        <div className='enter'>
+                            <p className='t_enter'>УВІЙТИ</p>
+                        </div>
+                    </AnchorLink>
+                    <AnchorLink id="#!" className='l_registration'>
+                        <div className='registration'>
+                            <p className='t_registration'>РЕЄСТРАЦІЯ</p>
+                        </div>
+                    </AnchorLink>
+                    </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </body>
         </div>
-        <Footer/>
+        <FooterLogin/>
         </>
      );
 }
