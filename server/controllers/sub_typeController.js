b92bce8142d5a2523533388d095ca9c18640188e
Artem Kruhliak <ar.krugliak@gmail.com>
2024-05-05 18:42:36 +0300

    login work

--- server/controllers/sub_typeController.js
+++ server/controllers/sub_typeController.js
@@ -3,12 +3,15 @@ const ApiError = require("../error/ApiError");
 
 class SubTypeController {
   async create(req, res) {
-    const { name, price } = req.body;
-    const sub_type = await Subscribe_types.create({ name, price });
+    const { name, price, duration } = req.body;
+    const sub_type = await Subscribe_types.create({ name, price, duration });
     return res.json(sub_type);
   }
 
-  async getALL(req, res) {}
+  async getALL(req, res) {
+    const sub_types = await Subscribe_types.findAll();
+    return res.json(sub_types);
+  }
 }
 
 module.exports = new SubTypeController();
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,71 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    try {
+      const { username, email, password, sex, age, weight, height, role } =
+        req.body;
 
-  async login(req, res) {}
+      /*const { profile_photo } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      profile_photo.mv(path.resolve(__dirname, "..", "static", fileName));*/
+      if (
+        !email ||
+        !password ||
+        !username ||
+        !sex ||
+        !age ||
+        !weight ||
+        !height
+      ) {
+        return next(ApiError.badRequest("You don`t fill all info"));
+      }
+      const candidate = await User.findOne({ where: { email } });
+      if (candidate) {
+        return next(ApiError.badRequest("Use another email"));
+      }
+      const hashPassword = await bcrypt.hash(password, 5);
+      const user = await User.create({
+        email,
+        role,
+        username,
+        sex,
+        age,
+        weight,
+        height,
+        password: hashPassword,
+      });
+      const token = generateJwt(user.id, user.email, user.role);
+      return res.json({ token });
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal("User with thi email not found"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Incorrect password"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
     const { id } = req.query;
