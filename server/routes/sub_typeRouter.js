9b405e42a9b3de23fe47cf4b7677762effc3acee
Artem Kruhliak <ar.krugliak@gmail.com>
2024-05-05 20:29:40 +0300

    UserController

--- server/routes/sub_typeRouter.js
+++ server/routes/sub_typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const sub_typeController = require("../controllers/sub_typeController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post("/", sub_typeController.create);
+router.post("/", checkRole("ADMIN"), sub_typeController.create);
 router.get("/", sub_typeController.getALL);
 
 module.exports = router;
