07a3f1871db145f60e5e807ade249c19844c88db
velichko <maksimvelichko16@gmail.com>
2024-05-14 11:08:50 +0300

    deleting the second registration

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Registration from "./pages/Registration"
-import RegistrationTwo from "./pages/RegistrationTwo"
 import Office from "./pages/Office"
 import Progress from "./pages/Progress"
-import { FOOD_ROUTE, HOME_ROUTE, LOGIN_ROUTE, OFFICE_ROUTE, PROGRESS_ROUTE, REGISTRATIONTWO_ROUTE, REGISTRATION_ROUTE } from "./utils/consts"
+import { FOOD_ROUTE, HOME_ROUTE, LOGIN_ROUTE, OFFICE_ROUTE, PROGRESS_ROUTE, REGISTRATION_ROUTE } from "./utils/consts"
 import Food from "./pages/Food"
 
 export const authRoutes = [
@@ -36,10 +35,6 @@ export const publicRoutes = [
         path: REGISTRATION_ROUTE,
         Component: Registration
     },
-    {
-        path: REGISTRATIONTWO_ROUTE,
-        Component: RegistrationTwo
-    }
 
 ]
     
